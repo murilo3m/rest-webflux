e0bf7825547aec5c6ab746ef6a9fdee9495eec2a
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2019-05-24 17:56:06 -0300

    First commit

---
+++ src/main/java/com/murilo3m/apirest/webflux/WebfluxApplication.java
@@ -0,0 +1,14 @@
+package com.murilo3m.apirest.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxApplication.class, args);
+		System.out.println("Olá Mundo com WebFlux!");
+	}
+
+}
